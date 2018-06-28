acc880c7d1850ed2624d5a2a8118b68635e0c448
DenysV <denzelSpain@gmail.com>
2018-06-28 16:45:28 +0200

    Fit mistake with button Save

--- tarea/views.py
+++ tarea/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import redirect
 from .models import Tarea
 from .forms import TareaForm
 
+from django.http import HttpResponseRedirect
+
 def tarea_list(request):
     tareas = Tarea.objects.filter(created_date__lte = timezone.now()).order_by('created_date')
     return render(request, 'tarea/tarea_list.html', {'tareas' : tareas})
@@ -28,12 +30,13 @@ def tarea_new(request):
 def tarea_edit(request, pk):
     tarea = get_object_or_404(Tarea, pk = pk)
     if request.method == "POST":
-        form = TareaForm(request.Tarea, instance = tarea)
+        form = TareaForm(request.POST, instance = tarea)
         if form.is_valid():
             tarea = form.save(commit = False)
             tarea.author = request.user
             tarea.created_date = timezone.now()
             tarea.save()
+            #return HttpResponseRedirect('tarea_detail')
             return redirect('tarea_detail', pk = tarea.pk)
     else:
         form = TareaForm(instance = tarea)
