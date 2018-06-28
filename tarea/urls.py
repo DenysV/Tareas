32f94033150a866febc15c00ada8fa2c3b32d6a6
DenysV <denzelSpain@gmail.com>
2018-06-28 11:13:05 +0200

    Create new and edit tareas

--- tarea/urls.py
+++ tarea/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.tarea_list, name = 'tarea_list'),
+
     url(r'^tarea/(?P<pk>\d+)/$', views.tarea_detail, name = 'tarea_detail'),
+
+    url(r'^tarea/(?P<pk>\d+)/edit/$', views.tarea_edit, name = 'tarea_edit'),
+
     url(r'^tarea/new/$', views.tarea_new, name = 'tarea_new'),
 ]
--- tarea/views.py
+++ tarea/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Tarea
-
 from .forms import TareaForm
 
 def tarea_list(request):
@@ -12,7 +10,7 @@ def tarea_list(request):
 
 def tarea_detail(request, pk):
     tarea = get_object_or_404(Tarea, pk = pk)
-    return render(request, 'tarea/tarea_detail.html', {'tarea':tarea})
+    return render(request, 'tarea/tarea_detail.html', {'tarea' : tarea})
 
 def tarea_new(request):
     if request.method == 'POST':
@@ -25,4 +23,18 @@ def tarea_new(request):
             return redirect('tarea_detail', pk = tarea.pk)
     else:
         form = TareaForm()
+    return render(request, 'tarea/tarea_edit.html', {'form':form})
+
+def tarea_edit(request, pk):
+    tarea = get_object_or_404(Tarea, pk = pk)
+    if request.method == "POST":
+        form = TareaForm(request.Tarea, instance = tarea)
+        if form.is_valid():
+            tarea = form.save(commit = False)
+            tarea.author = request.user
+            tarea.created_date = timezone.now()
+            tarea.save()
+            return redirect('tarea_detail', pk = tarea.pk)
+    else:
+        form = TareaForm(instance = tarea)
     return render(request, 'tarea/tarea_edit.html', {'form':form})
